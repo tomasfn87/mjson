49475c666d2f0e90ebb44dd7c32f6a60bf1ab235
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-02-08 00:31:07 -0300

    Adding files: 'minify.py' and 'minify.sh': remove empty spaces, tabs ('\t') and line escapes ('\n') outside strings ("" or '').

---
+++ minify.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+toGreen () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;32m" text "\033[0m" }'
+}
+
+toRed () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;31m" text "\033[0m" }'
+}
+
+toYellow () { gawk -v text=$1 'BEGIN {
+    printf "%s", "\033[1;33m" text "\033[0m" }'
+}
+
+if [ $1 == $2 ]
+then
+    echo "$(toRed ERROR): cannot overwrite source file $(toYellow $2)";
+    echo "Please rename the minified file or save it to another folder";
+    exit 1
+fi;
+
+echo "Minifying file..."
+python3 minify.py $1 | cat > $2;
+echo "$(toGreen DONE)". "File saved to $(toYellow $2)";
+exit 0;
