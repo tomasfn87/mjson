febfcfa024597c563c043b82154d5b86a973ae3b
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-10-23 20:16:08 -0300

    Changed file 'minify_json.sh': bug correction (missing ;).

--- minify_json.sh
+++ minify_json.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-toGreen () { gawk -v text="$1" 'BEGIN {
+toGreen() { gawk -v text="$1" 'BEGIN {
     printf "%s", "\033[1;32m" text "\033[0m" }';
 }
 
-toRed () { gawk -v text="$1" 'BEGIN {
+toRed() { gawk -v text="$1" 'BEGIN {
     printf "%s", "\033[1;31m" text "\033[0m" }';
 }
 
-toYellow () { gawk -v text="$1" 'BEGIN {
+toYellow() { gawk -v text="$1" 'BEGIN {
     printf "%s", "\033[1;33m" text "\033[0m" }';
 }
 
-verifyJson () {
+verifyJson() {
     echo $(python3 /usr/local/lib/verify-minify-json/verify_json.py "$1");
 }
 
@@ -21,14 +21,14 @@ OUTPUT="$2";
 NEW_FILE=maybe;
 
 if [ "$OUTPUT" == "$INPUT" ]; then
-    echo "$(toRed ERROR): source file and output file cannot be the same"
+    echo "$(toRed ERROR): source file and output file cannot be the same";
     exit 3; fi;
 
-echo -n " * Checking if source file exists...    "
+echo -n " * Checking if source file exists...    ";
 if [ -f "$INPUT" ]; then
     echo "[$(toGreen OK)]";
 else
-    echo "$(toRed ERROR): file not found"
+    echo "$(toRed ERROR): file not found";
     exit 6; fi;
 
 echo -n " * Checking source file integrity...    ";
@@ -43,11 +43,11 @@ elif [ $(verifyJson "$INPUT") == 2 ]; then
 else
     echo "[$(toGreen OK)]"; fi;
 
-if [[ "$OUTPUT" == "" || "$OUTPUT" == " " ]] then
+if [[ "$OUTPUT" == "" || "$OUTPUT" == " " ]]; then
     echo "$(toRed ERROR): please specify minified JSON path";
     exit 5; fi;
 
-echo -n " * Checking if target file exists...    "
+echo -n " * Checking if target file exists...    ";
 if [ -w "$OUTPUT" ]; then
     echo "[$(toGreen OK)] File will be overwritten...";
 else
@@ -55,25 +55,26 @@ else
     NEW_FILE=yes;
     echo "[NO] File will be created..."; fi;
 
-echo -n " * Checking target file extension...    "
-if [ $(verifyJson "$OUTPUT") == 1 ]; then
-    echo "$(toRed ERROR)";
-    echo "Target file extension must be '$(toYellow .json)'"
+echo -n " * Checking target file extension...    ";
+if [ `verifyJson "$OUTPUT"` == 1 ]; then
+    echo "`toRed ERROR`";
+    echo "Target file extension must be '`toYellow .json`'";
     if [ "$NEW_FILE" == yes ]; then
         rm "$OUTPUT";
         exit 4; fi;
 else
     echo "[$(toGreen OK)]"; fi;
 
-echo -n " * Minifying JSON file...               "
+echo -n " * Minifying JSON file...               ";
 python3 /usr/local/lib/verify-minify-json/minify.py "$INPUT" | cat > "$OUTPUT";
 echo "[$(toGreen OK)]";
 
-echo -n " * Checking output file...              "
+echo -n " * Checking output file...              ";
 if [ $(verifyJson "$OUTPUT") != 0 ]; then
-    echo "$(toRed ERROR)"
+    echo "$(toRed ERROR)";
     echo "Minified JSON validation failed";
 else
-    echo "[$(toGreen OK)]";
-    echo; echo "File saved to $(toYellow $(realpath "$OUTPUT"))"; fi;
+    echo "[`toGreen OK`]";
+
+echo; echo "File saved to `toYellow $(realpath "$OUTPUT")`"; fi;
 exit 0;
