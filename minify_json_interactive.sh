a3b9a6b120984326c44f9283a59825bfc154f5fa
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-11-19 17:32:57 -0300

    Changed file 'minify_json_interactive.sh': removed unnecessary "" (double brackets) and replaced $() with `` when possible.

--- minify_json_interactive.sh
+++ minify_json_interactive.sh
@@ -21,23 +21,23 @@ parse~FromHome() {
 }
 
 verifyJson() {
-    echo $(python3 /usr/local/lib/verify-minify-json/verify_json.py "$1");
+    echo `python3 /usr/local/lib/verify-minify-json/verify_json.py "$1"`;
 }
 
 openJson() {
-    echo -n " * Checking if source file exists...    ";
+    echo -n ' * Checking if source file exists...    ';
 
-    if [[ ${1:0:2} == "~/" ]]; then
+    if [[ ${1:0:2} == '~/' ]]; then
         MINIFIEDJSON=`parseHomeFrom~ $MINIFIEDJSON`; fi;
 
     if [ -r `realpath -q "$1"` ]; then
         echo "[`toGreen OK`]";
     else
         echo "`toRed ERROR`: file not found";
-        echo; echo "Please choose an existing file:";
+        echo; echo 'Please choose an existing file:';
         return 3; fi;
 
-    echo -n " * Checking JSON file integrity...      ";
+    echo -n ' * Checking JSON file integrity...      ';
 
     if [ `verifyJson "realpath -q $1"` == 1 ]; then
         echo "`toRed ERROR`: not a JSON file";
@@ -45,32 +45,32 @@ openJson() {
         return 1;
     elif [ `verifyJson "$1"` == 2 ]; then
         echo "`toRed ERROR`: invalid JSON data";
-        echo; echo "Please choose a valid JSON file:";
+        echo; echo 'Please choose a valid JSON file:';
         return 2;
     else
         echo "[`toGreen OK`]";
         return 0; fi;
 }
 
-echo "Choose JSON file to be minified:";
-read -ei "$(echo )" JSONFILE;
+echo 'Choose JSON file to be minified:';
+read -ei "`echo `" JSONFILE;
 openJson "$JSONFILE";
 
 while [ $? != 0 ]; do
     read -ei "`echo "$JSONFILE"`" JSONFILE;
-openJson "$JSONFILE"; done;
+    openJson "$JSONFILE"; done;
 
 HOME_SHORTCUT=1;
-if [[ ${JSONFILE:0:2} == "~/" ]]; then
+if [[ "${JSONFILE:0:2}" == "~/" ]]; then
     HOME_SHORTCUT=0; fi;
 
-echo; echo "Save minified JSON file to:";
+echo; echo 'Save minified JSON file to:';
 read -ei "$JSONFILE" MINIFIEDJSON;
 
 while [ "$MINIFIEDJSON" == "$JSONFILE" ] || [ `verifyJson "$MINIFIEDJSON"` == 1 ]; do
     if [ "$MINIFIEDJSON" == "$JSONFILE" ]; then
         echo "`toRed ERROR`: cannot overwrite source file `toYellow "$JSONFILE"`";
-        echo; echo "Please rename the target minified file or save it to another folder:";
+        echo; echo 'Please rename the target minified file or save it to another folder:';
         read -ei "`echo "$MINIFIEDJSON"`" MINIFIEDJSON;
     elif [ `verifyJson "$MINIFIEDJSON"` == 1 ]; then
         echo; echo "`toRed ERROR`: minified JSON file extension must be '`toYellow .json`'";
@@ -85,12 +85,12 @@ else
 if [[ $HOME_SHORTCUT == 0 ]]; then
     MINIFIEDJSON=`parseHomeFrom~ $MINIFIEDJSON`; fi;
 
-echo -n " * Checking if target file exists...    ";
+echo -n ' * Checking if target file exists...    ';
 
 if [ -w `realpath -q "$MINIFIEDJSON"` ]; then
     echo "[`toGreen OK`]";
 
-    echo; echo -n "Is file overwriting allowed? (Y/N) ";
+    echo; echo -n 'Is file overwriting allowed? (Y/N) ';
     read OPTION;
     OPTIONS=(Y y N n);
     YES=(Y y);
@@ -100,14 +100,14 @@ if [ -w `realpath -q "$MINIFIEDJSON"` ]; then
         echo; echo -n "`toRed ERROR`: invalid option, choose [Y]es or [N]o: ";
         read OPTION; done;
 
-    if [[ "${YES[*]}" =~ "${OPTION}" ]]; then
-        echo " * File will be overwritten...";
-    elif [[ "${NO[*]} " =~ "${OPTION}" ]]; then
+    if [[ ${YES[*]} =~ ${OPTION} ]]; then
+        echo ' * File will be overwritten...';
+    elif [[ ${NO[*]} =~ ${OPTION} ]]; then
         NEWFILE="$MINIFIEDJSON";
         while [[ "$NEWFILE" == "$MINIFIEDJSON"
             || -w `realpath -q "$NEWFILE"`
             || `verifyJson "$NEWFILE"` == 1 ]]; do
-            echo; echo "Save minified JSON file to: ";
+            echo; echo 'Save minified JSON file to: ';
             if [[ $HOME_SHORTCUT == 0 ]]; then
                 NEWFILE=`parse~FromHome $NEWFILE`; fi;
             read -ei `echo "$NEWFILE"` NEWFILE;
@@ -119,16 +119,16 @@ if [ -w `realpath -q "$MINIFIEDJSON"` ]; then
                 NEWFILE="`parseHomeFrom~ $NEWFILE`"; fi;
             if [[ "$NEWFILE" == "$MINIFIEDJSON" || -w `realpath -q "$NEWFILE"` ]]; then
                 echo "`toRed ERROR`: files already exists and overwriting is disabled.";
-                echo; echo "Please rename the file or save it to another folder:";
+                echo; echo 'Please rename the file or save it to another folder:';
             elif [ `verifyJson "$NEWFILE"` == 1 ]; then
                 echo "`toRed ERROR`: minified JSON file extension must be '`toYellow .json`'";
-                echo; echo "Please rename the target minified file or save it to another folder:";
+                echo; echo 'Please rename the target minified file or save it to another folder:';
             else
                 break; fi; done;
 
         touch $(realpath -q "$NEWFILE");
 
-        echo " * File will be created...";
+        echo ' * File will be created...';
 
         MINIFIEDJSON="$NEWFILE";
         MINIFIEDJSON=`parseHomeFrom~ $MINIFIEDJSON`;
@@ -140,11 +140,11 @@ elif [ ! -f "`realpath -q "$MINIFIEDJSON"`" ]; then
     touch "`realpath -q $MINIFIEDJSON`"; fi;
 
 JSONFILE=`parseHomeFrom~ "$JSONFILE"`;
-echo -n " * Minifying JSON file...               ";
+echo -n ' * Minifying JSON file...               ';
 python3 /usr/local/lib/verify-minify-json/minify.py "`realpath -q "$JSONFILE"`" | cat > "`realpath -q "$MINIFIEDJSON"`";
 echo "[`toGreen OK`]";
 
-echo -n " * Checking minified output file...     ";
+echo -n ' * Checking minified output file...     ';
 if [ `verifyJson "$MINIFIEDJSON"` != 0 ]; then
     echo "`toRed ERROR`: minified JSON validation failed";
 else
